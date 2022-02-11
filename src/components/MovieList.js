38627b5da89f2b925d0bd2601ec91cdf2b220e93
loonatiq <86408147+loonatiq@users.noreply.github.com>
2022-02-11 23:35:16 +0900

    modifyCss

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,17 +1,25 @@
 import { Link } from "react-router-dom";
 import "./MovieList.css";
+import Rating from "./Rating";
 
 function MovieList({ movies }) {
   return (
     <>
       {movies.map((value) => (
         <div key={value.id} className="movielist">
-          <div key={value.id}>
-            <img src={value.medium_cover_image} alt={value.title}></img>
-            <h1>
-              <Link to={`/movie/${value.id}`}>{value.title_long}</Link>
-            </h1>
-          </div>
+          <Link to={`/movie/${value.id}`}>
+            <div key={value.id}>
+              <img src={value.medium_cover_image} alt={value.title}></img>
+              <div className="movielist_info">
+                <h1>{value.title_long}</h1>
+                <p>{value.summary.slice(0, 150)}...</p>
+                <div>{value.rating}</div>
+                <div>
+                  <Rating rating={value.rating} />
+                </div>
+              </div>
+            </div>
+          </Link>
         </div>
       ))}
     </>
---
+++ src/components/Rating.js
@@ -0,0 +1,20 @@
+import "./Rating.css";
+
+function Star({ selected }) {
+  const className = `Rating-star ${selected ? "selected" : ""}`;
+  return <span className={className}>★</span>;
+}
+
+const RATINGS = [1, 2, 3, 4, 5];
+
+function Rating({ rating }) {
+  return (
+    <>
+      {RATINGS.map((value) => (
+        <Star key={value} selected={value <= (rating + 1) / 2} />
+      ))}
+      <span>({rating})</span>
+    </>
+  );
+}
+export default Rating;
